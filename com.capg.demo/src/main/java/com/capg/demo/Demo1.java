69817b6ff57e4e3eb980fd181e98afe8196b87ef
Devu Om SriDatta Sai <Devu Om SriDatta Sai@DESKTOP-IB6LKDT>
2021-07-31 14:35:38 +0530

    if block add

--- com.capg.demo/src/main/java/com/capg/demo/Demo1.java
+++ com.capg.demo/src/main/java/com/capg/demo/Demo1.java
@@ -4,8 +4,9 @@ public class Demo1 {
 static Double num1;
 	public static void main(String[] args) {
 	
+		if (num1 !=null) {
 			String body=String.format("%.2f", num1).replace(".", "");
-			
+			}
 		
 
 	}
