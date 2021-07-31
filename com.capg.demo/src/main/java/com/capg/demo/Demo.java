349b539608bded23b45f2681b3eca74c915ecb38
Devu Om SriDatta Sai <Devu Om SriDatta Sai@DESKTOP-IB6LKDT>
2021-07-31 14:15:14 +0530

    oneline commit

---
+++ com.capg.demo/src/main/java/com/capg/demo/Demo.java
@@ -0,0 +1,16 @@
+package com.capg.demo;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		Double str=0.0;
+		if(str==0)
+		{
+			System.out.println("equal");
+		}else {
+			System.out.println("not equal");
+		}
+
+	}
+
+}
---
+++ com.capg.demo/src/main/java/com/capg/demo/Demo1.java
@@ -0,0 +1,12 @@
+package com.capg.demo;
+
+public class Demo1 {
+static Double num1;
+	public static void main(String[] args) {
+	
+		String body=String.format("%.2f", num1).replace(".", "");
+		
+
+	}
+
+}
