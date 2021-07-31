fcd13b1cca9ae17a804d5c8b451267f13f8c6ea0
Devu Om SriDatta Sai <Devu Om SriDatta Sai@DESKTOP-IB6LKDT>
2021-07-31 14:25:17 +0530

    main branch if block commit

--- com.capg.demo/src/main/java/com/capg/demo/Demo1.java
+++ com.capg.demo/src/main/java/com/capg/demo/Demo1.java
@@ -4,7 +4,9 @@ public class Demo1 {
 static Double num1;
 	public static void main(String[] args) {
 	
-		String body=String.format("%.2f", num1).replace(".", "");
+		if (num1 !=null) {
+			String body=String.format("%.2f", num1).replace(".", "");
+			}
 		
 
 	}
