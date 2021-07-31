009ba70ce0c1b1b7dc0b4ab8970e9f29036c8624
Devu Om SriDatta Sai <Devu Om SriDatta Sai@DESKTOP-IB6LKDT>
2021-07-31 14:30:31 +0530

    revert if

--- com.capg.demo/src/main/java/com/capg/demo/Demo1.java
+++ com.capg.demo/src/main/java/com/capg/demo/Demo1.java
@@ -4,9 +4,8 @@ public class Demo1 {
 static Double num1;
 	public static void main(String[] args) {
 	
-		if (num1 !=null) {
 			String body=String.format("%.2f", num1).replace(".", "");
-			}
+			
 		
 
 	}
